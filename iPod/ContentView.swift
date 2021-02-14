959717b539d37c09b6804ce05583b1db53c8ecf7
Eric Groom <bcgroom@gmail.com>
2021-02-14 10:14:00 -0800

    messing around with drag gesture

--- iPod/ContentView.swift
+++ iPod/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var location: CGPoint?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text(locationText)
+                .padding()
+            Circle()
+                .foregroundColor(.gray)
+                .frame(width: 300, height: 300, alignment: .center)
+                .gesture(
+                    DragGesture(minimumDistance: 10.0, coordinateSpace: .local)
+                        .onChanged { value in
+                            self.location = value.location
+                        }
+                )
+        }
+    }
+    
+    var locationText: String {
+        location.map { point in
+            "\(point.x), \(point.y)"
+        } ?? "none"
     }
 }
 
