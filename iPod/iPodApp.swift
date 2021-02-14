24242be7cf759a41cd2d541d0036728ebd3916ba
Eric Groom <bcgroom@gmail.com>
2021-02-14 10:05:22 -0800

    Initial Commit

---
+++ iPod/iPodApp.swift
@@ -0,0 +1,17 @@
+//
+//  iPodApp.swift
+//  iPod
+//
+//  Created by Eric Groom on 2/14/21.
+//
+
+import SwiftUI
+
+@main
+struct iPodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
